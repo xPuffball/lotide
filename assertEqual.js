927929c8608c94836110f54ff39f3b9707f6d21d
Jaemin Han <j4emin.han@gmail.com>
2021-04-26 17:08:42 +0000

    Added assertEqual.js, a verbose console.assert type function

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed 😇: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed 😈: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
