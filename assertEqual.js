92cb4c5498a7f0a478195f201a3b5e35223dd495
Jaemin Han <j4emin.han@gmail.com>
2021-04-26 17:14:54 +0000

    Refactored assertEqual.js to use template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed 😇: " + actual + " === " + expected);
+    console.log(`Assertion Passed 😇: ${actual} === ${expected}`);
   } else {
-    console.log("Assertion Failed 😈: " + actual + " !== " + expected);
+    console.log(`Assertion Failed 😈: ${actual} !== ${expected}`);
   }
 };
 
