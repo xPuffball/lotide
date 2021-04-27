a473cd9b0a9c8e48397305f8952f1d8c14857d1a
Jaemin Han <j4emin.han@gmail.com>
2021-04-27 23:24:33 +0000

    Completed letterPositions, a function that returns the positions of the letters in a sentence

---
+++ letterPositions.js
@@ -0,0 +1,18 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]]){
+        results[sentence[i]].push(i);
+      }
+      else {
+        results[sentence[i]] = [i];
+      }
+    } 
+  }
+
+
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
