a39ef02cf97467ed0e6c182300e3eaee4ce954fc
Jaemin Han <j4emin.han@gmail.com>
2021-04-28 00:00:52 +0000

    created eqObjects for comparing two objects (primitive types)

---
+++ eqObjects.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed 😇: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed 😈: ${actual} !== ${expected}`);
+  }
+};
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let key in object1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false); 
