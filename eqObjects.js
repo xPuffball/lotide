a0ff178db0499b601c51a8a0665d67fe793c7d6a
Jaemin Han <j4emin.han@gmail.com>
2021-04-28 00:25:50 +0000

    Updated eqObjects compatibility to support objects

--- eqObjects.js
+++ eqObjects.js
@@ -6,12 +6,38 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(arr1, arr2){
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  if(arr1.length === 0){
+    return true;
+  }
+  else if(Array.isArray(arr1[0]) && Array.isArray(arr2[0])){
+    return eqArrays(arr1[0], arr2[0]) && eqArrays(arr1.slice(1), arr2.slice(1));
+  }
+  else if(arr1[0] !== arr2[0]){
+    return false;
+  }
+  else{
+    return eqArrays(arr1.slice(1), arr2.slice(1))
+  }
+}
+
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
   for (let key in object1) {
-    if (object1[key] !== object2[key]) {
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    }
+    else if (typeof object1[key] === 'object' && object1[key] !== null){
+      return eqObjects(object1[key], object2[key]);
+    }
+    else if (object1[key] !== object2[key]) {
       return false;
     }
   }
@@ -24,3 +50,21 @@ assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false); 
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
+
+const xy = { c: {a: 1}, d: {b: 2}};
+const yx = { d: {b: 2}, c: {a: 1}};
+assertEqual(eqObjects(xy, yx), true)
+
+const yz = { d: {b: 2}, c: {d: 2}};
+assertEqual(eqObjects(xy, yz), false)
+
+const ij = { a: {b: {c: 1}}, d: {e: {f : 2}}}
+const ji = { d: {e: {f : 2}}, a: {b: {c: 1}}}
+assertEqual(eqObjects(ij, ji), true);
