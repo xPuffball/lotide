bd0076beb45dd248bfb65f56886e7d05a3ffb358
Jaemin Han <j4emin.han@gmail.com>
2021-04-29 17:50:18 +0000

    Added takeUntil.js, a function that returns an array slice

---
+++ takeUntil.js
@@ -0,0 +1,18 @@
+const takeUntil = function(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return array.slice(0, i);
+    }
+  }
+  return "No element returns true for the callback!"
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
