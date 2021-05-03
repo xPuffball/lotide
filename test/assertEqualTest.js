e263ca093259ac16fb16c43d120cfd4e74129966
Jaemin Han <j4emin.han@gmail.com>
2021-05-03 19:19:33 +0000

    refactoring into dry code

---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual')
+
+// describe('assertEqual', function () {
+//   it ("should return true if passwed two identical elements", function () {
+//     let ele1 = "lighthouse";
+//     let ele2 = "lighthouse";
+//     assert.equal(assertEqual(ele1, ele2), true);
+//   })
+// })
